20b31c99caccecea69e4134e92e6fac8ae38dbe0
Swarna'sCodes <swarna.jst9@gmail.com>
2025-11-11 14:24:09 +0530

    Piredict pipeline

---
+++ src/pipeline/predict_pipeline.py
@@ -0,0 +1,52 @@
+import os
+import sys
+import pandas as pd
+from src.exception import CustomException
+from src.utils import load_object
+
+
+class PredictPipeline:
+    def __init__(self):
+        pass
+
+    def predict(self,features):
+        try:
+            model_path=os.path.join("artifacts","model.pkl")
+            preprocessor_path=os.path.join('artifacts','preprocessor.pkl')
+            print("Before Loading")
+            model=load_object(file_path=model_path)
+            preprocessor=load_object(file_path=preprocessor_path)
+            print("After Loading")
+            data_scaled=preprocessor.transform(features)
+            preds=model.predict(data_scaled)
+            return preds
+        
+        except Exception as e:
+            raise CustomException(e,sys)
+
+
+
+class CustomData:
+    def __init__(  self,
+        Product_Category: str,
+        Payment_Method: str,
+        Quantity_Purchased: float):
+
+        self.Product_Category = Product_Category
+
+        self.Payment_Method = Payment_Method
+
+        self.Quantity_Purchased = Quantity_Purchased
+
+    def get_data_as_data_frame(self):
+        try:
+            custom_data_input_dict = {
+                "Product_Category": [self.Product_Category],
+                "Payment_Method": [self.Payment_Method],
+                "Quantity_Purchased": [self.Quantity_Purchased],
+            }
+
+            return pd.DataFrame(custom_data_input_dict)
+
+        except Exception as e:
+            raise CustomException(e, sys)
